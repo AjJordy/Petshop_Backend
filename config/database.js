a5d0d437408835cee6149cdfc27c5d5ce6543481
Jordy Aparecido Faria de Araújo <jordyfaria@hotmail.com>
2019-01-04 11:50:11 -0200

    Sequelize

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  username: 'root',
+  password: 'rootroot',
+  database: 'crud',
+  host: '127.0.0.1',
+  dialect: 'mysql',
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+const { User } = require('./app/models');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// User.create({ name: 'Claudio', email: 'claudio@rocketseat.com.br', password: '123456' });
+
+app.post('/register', async (req, res) => {
+    const user = await User.create(req.body);
+    res.json(user);
+});
+
+app.get('/find/:id', (req, res) => {
+    res.json('Im in register');
+});
+
+app.get('/findall', (req, res) => {
+    res.json('Im in Find All');
+});
+
+app.put('/update/:id', (req, res) => {
+    res.json('Im in update');
+});
+
+app.delete('/delete/:id', (req, res) => {
+    res.json('Im in delete');
+});
+
+app.listen(3000);
